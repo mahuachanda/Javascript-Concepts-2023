f392553798a2578af8491abf09938ab0179fc78d
Mahua Chanda P <mahuachanda@gmail.com>
2023-05-20 18:13:13 +0530

    Added Basic_JSconcepts

---
+++ Basic_JSconcepts/concepts.js
@@ -0,0 +1,5 @@
+let results = prompt("How old are you?", 34);
+console.log(results);
+
+let confirmMessage = confirm("Are you sure to cancel or continue?");
+console.log(confirmMessage); //true if pressed OK
